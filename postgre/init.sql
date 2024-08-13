2dd5b70b0668d55a70d94acb27ffdad6cf6be42c
Jorge Torrado <jorgeetorradoforero@gmail.com>
2024-08-13 17:36:57 -0500

    actualizamos el postgre

--- postgre/init.sql
+++ postgre/init.sql
@@ -1,8 +1,25 @@
-CREATE TABLE public.formulario (
+CREATE TABLE public.arrendadores (
     id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
     nombre VARCHAR(100),
-    apellidos VARCHAR(100),
-    correo VARCHAR(100),
-    semestre INTEGER,
-    descripcion VARCHAR(500)
+    apellido VARCHAR(100),
+    correo VARCHAR(100)
+);
+CREATE TABLE public.arrendatarios (
+    id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    nombre VARCHAR(100),
+    apellido VARCHAR(100),
+    correo VARCHAR(100)
+);
+CREATE TABLE public.propiedades (
+    id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    id_arrendatario INTEGER REFERENCES public.arrendatarios(id),
+    nombrePropiedad VARCHAR(100)
+);
+CREATE TABLE public.contratos (
+    id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    id_propiedad INTEGER REFERENCES public.propiedades(id),
+    id_arrendador INTEGER REFERENCES public.arrendadores(id),
+    precio NUMERIC(10,2),
+    fechaInicio TIMESTAMP,
+    fechaFinal TIMESTAMP
 );
