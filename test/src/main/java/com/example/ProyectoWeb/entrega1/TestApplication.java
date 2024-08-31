f3bc4e5c0fc4f75acff61fe5fe29000322ffa0e8
Jorge Torrado <jorgeetorradoforero@gmail.com>
2024-08-30 22:47:49 -0500

    hay prueba que no se ha probado XD

--- test/src/main/java/com/example/ProyectoWeb/entrega1/TestApplication.java
+++ test/src/main/java/com/example/ProyectoWeb/entrega1/TestApplication.java
@@ -2,10 +2,8 @@ package com.example.ProyectoWeb.entrega1;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan(basePackages = {"com.example.ProyectoWeb.entrega1.service","com.example.ProyectoWeb.entrega1.config","com.example.ProyectoWeb.entrega1.dto","com.example.ProyectoWeb.entrega1.repository","com.example.ProyectoWeb.entrega1.model","com.example.ProyectoWeb.entrega1.prueba.controllers"})
 public class TestApplication {
 
 	public static void main(String[] args) {
--- test/src/main/java/com/example/ProyectoWeb/entrega1/model/Arrendadores.java
+++ test/src/main/java/com/example/ProyectoWeb/entrega1/model/Arrendadores.java
@@ -8,32 +8,5 @@ import jakarta.persistence.Table;
 @Table(name = "arrendadores")
 public class Arrendadores extends Usuario {
 
-    @Column(name = "promedio", nullable = false)
-    private Float promedio = 0.0f;
-
-    @Column(name = "canti_calif", nullable = false)
-    private Integer cantiCalif = 0;
-
-    // Getters y Setters
-    @Override
-    public Float getPromedio() {
-        return this.promedio; 
-    }
-
-    @Override
-    public void setPromedio(Float promedio) {
-        this.promedio = promedio; 
-    }
-
-    @Override
-    public Integer getCantiCalif() {
-        return this.cantiCalif; 
-    }
-
-    @Override
-    public void setCantiCalif(Integer cantiCalif) {
-        this.cantiCalif = cantiCalif;
-    }
-
 }
 
